3d2c524f2e208c13492d5e136027d6578cfc5f6f
Hdidogs <a.rohee@lprs.fr>
2024-03-05 14:45:54 +0100

    Feat : page Inscription #3

--- vue/user/Inscription.php
+++ vue/user/Inscription.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Inscription</title>
+</head>
+<body>
+<div class="inscription">
+    <form method="post" action="../../src/controleur/TraitementUser.php">
+        <input type="text" name="nom" placeholder="Nom" required>
+        <br>
+        <input type="text" name="prenom" placeholder="Prenom" required>
+        <br>
+        <input type="text" name="ville" placeholder="Adresse Mail" required>
+        <br>
+        <input type="date" name="date" placeholder="Date de Naissance" required>
+        <br>
+        <input type="email" name="mail" placeholder="Adresse Mail" required>
+        <br>
+        <input type="password" name="mdp" placeholder="Mot de Passe" required>
+        <br>
+        <input type="password" name="remdp" placeholder="Confirmer Mot de Passe" required>
+        <br>
+        <a href="Connexion.php">Vous avez déjà un compte ?</a>
+        <br>
+        <input type="submit" name="inscription" value="Inscription">
+    </form>
+</div>
+</body>
+</html>
 
