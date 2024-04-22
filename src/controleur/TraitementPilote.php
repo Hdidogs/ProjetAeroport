ebd9dcd19b8e7853134444a5e1e7b7b67d9d7ca9
Hdidogs <hdidogs.pro@gmail.com>
2024-04-22 23:25:38 +0200

    Feat : Ajout pilote

--- src/controleur/TraitementPilote.php
+++ src/controleur/TraitementPilote.php
@@ -6,7 +6,11 @@ if (isset($_POST['ajouterpilote'])) {
     $prenom = htmlspecialchars($_POST['prenom']);
     $date_naissance = htmlspecialchars($_POST['date_naissance']);
     $mail = htmlspecialchars($_POST['mail']);
-    $mdp = htmlspecialchars($_POST['mdp']);
+
+    $code = rand(100000, 999999);
+    $mdp = password_hash($code, PASSWORD_DEFAULT);
+    Mail::SENDMAIL($_SESSION['mail'], 'Nuage Air Pilote', 'Voici votre mot de passe temporaire veuillez le changer dès votre première connexion : ' . $code);
+
     $ville = htmlspecialchars($_POST['ville']);
     $fonction = htmlspecialchars($_POST['fonction']);
     $compagnie = htmlspecialchars($_POST['compagnie']);
